3f89618b7cb967d3192f8b7c2355af9e36a6d38e
zhmx1212 <zhmx1212@163.com>
2020-06-22 20:19:43 +0800

    快速实现repository类  变绿

--- src/test/java/com/twschool/practice/domain/GameRepository.java
+++ src/test/java/com/twschool/practice/domain/GameRepository.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Repository;
 public class GameRepository {
     private GuessNumberGame guessNumberGame = new GuessNumberGame(new AnswerGenerator());
 
-    public GuessNumberGame create() {
-        return null;
+    public GuessNumberGame create(){
+        guessNumberGame = new GuessNumberGame(new AnswerGenerator());
+        return guessNumberGame;
     }
 
-    public GuessNumberGame find() {
-        return null;
+    public GuessNumberGame find(){
+        return guessNumberGame;
     }
 
 }
